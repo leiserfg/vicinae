ba178c4b013ac54d75a08112b3c1f45b72d1bdf1
leiserfg <leiserfg@gmail.com>
2025-11-18 17:38:02 +0100

    Handle undefined images

--- typescript/api/src/api/image.ts
+++ typescript/api/src/api/image.ts
@@ -39,7 +39,7 @@ export const serializeProtoImage = (image: ImageLike): ui.Image => {
       };
     }
 
-    return { raw: payload.toString() };
+    return { raw: payload?.toString() };
   };
 
   if (image instanceof URL || typeof image === "string") {
